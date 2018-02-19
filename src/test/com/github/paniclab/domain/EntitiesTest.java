8c58c613106be92c781e0720dec1efbe89abe53f
kuzmin.s <adskayapochta@mail.ru>
2018-02-19 19:19:28 +0400

    добавлен тест

--- src/test/com/github/paniclab/domain/EntitiesTest.java
+++ src/test/com/github/paniclab/domain/EntitiesTest.java
@@ -1,5 +1,6 @@
 package com.github.paniclab.domain;
 
+import org.junit.jupiter.api.Test;
 import test.Customer;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,5 +16,22 @@ class EntitiesTest {
 
         assertEquals(original, copy);
         assertFalse(original == copy);
+    }
+
+    @Test
+    void getCopyOf_() {
+
+        Customer original = new Customer();
+        original.setAge(32);
+        original.setName("Влад");
+
+        Customer copy = Entities.getCopyOf(original);
+
+        assertEquals(original, copy);
+        assertFalse(original == copy);
+
+        copy.setName("Илья");
+
+        assertNotEquals(original, copy);
     }
 }
